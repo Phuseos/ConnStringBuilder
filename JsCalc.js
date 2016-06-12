c62dccaa8a97026d7edc52d112f008a7bc3cd4ea
Phus <Phuseos@gmail.com>
2016-06-12 09:11:40 +0200

    Added stylesheet, defined function for button

--- JsCalc.js
+++ JsCalc.js
@@ -1,2 +1,3 @@
-var hello = 'Hello World';
-document.write(hello);
+//$DoFunction -> Create string and output it in text / copy to clipboard for immediate usage
+
+var EventHandler = document.getElementById("btnCreatestring");
