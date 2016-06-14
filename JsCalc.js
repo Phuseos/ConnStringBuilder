3ccde166053f7842200af24e203f3fbba90d7702
frack <phuseos@gmail.com>
2016-06-14 21:07:03 +0200

    Updated CSS / HTML, added code to JavaScript to create MySQL like connection strings.

--- JsCalc.js
+++ JsCalc.js
@@ -1,3 +1,22 @@
 //$DoFunction -> Create string and output it in text / copy to clipboard for immediate usage
+function createString() {
+var OutputString = document.getElementById("txtString");
 
-var EventHandler = document.getElementById("btnCreatestring");
+var Server = document.getElementById("txtServer").value;
+var Database = document.getElementById("txtDatabase").value;
+var UID = document.getElementById("txtUID").value;
+var Password = document.getElementById("txtPwd").value;
+
+//Add diffrent items based on if they're filled or not
+myText = Server+';';
+
+//Needs a better check
+if (Database) { myText = Server+';'+Database+';'; }
+if (UID) { myText = Server+';'+Database+';'+UID+';'; }
+if (Password) { myText = Server+';'+Database+';'+UID+';'+Password+';'; }
+
+//myText = Server+';'+Database+';';
+
+OutputString.value = myText;
+}
+
