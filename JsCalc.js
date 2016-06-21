f34f15b25538be65b406d50e250a02ee23cd29f4
frack <phuseos@gmail.com>
2016-06-21 20:18:30 +0200

    More JavaScript addition

--- JsCalc.js
+++ JsCalc.js
@@ -6,12 +6,16 @@ var Server = document.getElementById("txtServer").value;
 var Database = document.getElementById("txtDatabase").value;
 var UID = document.getElementById("txtUID").value;
 var Password = document.getElementById("txtPwd").value;
+var StringType = document.getElementById("cboType").value;
+
+//alert(StringType);
+//if (StringType = "MySQL") { alert('MySQL!'); }
 
 //Add diffrent items based on if they're filled or not
 myText = Server+';';
 
 //Needs a better check
-if (!Server) { alert('Please enter at least a server adress'); }
+if (!Server) { alert('Please enter at least a server adress'); return; }
 
 if (Database) { myText = myText + Database + ';'; }
 if (UID) { myText = myText + UID + ';'; }
